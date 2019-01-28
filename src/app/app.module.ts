9ea81e38030af613c6d031f74a0f329257e40f40
JohnnyMatrixxx <JohnnyMatrixxx@users.noreply.github.com>
2019-01-28 15:07:12 -0400

    Added shared component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,7 @@ import { RecipesComponent } from './recipes/recipes.component';
 import { ShoppingListComponent } from './shopping-list/shopping-list.component';
 import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-edit.component';
 import { RecipeListComponent } from './recipes/recipe-list/recipe-list.component';
+import { SharedComponent } from './shared/shared.component';
 
 @NgModule({
   declarations: [
@@ -17,7 +18,8 @@ import { RecipeListComponent } from './recipes/recipe-list/recipe-list.component
     RecipesComponent,
     ShoppingListComponent,
     ShoppingEditComponent,
-    RecipeListComponent
+    RecipeListComponent,
+    SharedComponent
   ],
   imports: [
     BrowserModule,
